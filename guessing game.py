f8e67bbb9b90ba8300b81ea6f10b17db6728100d
Dualtach <96420971+dualtachof-skc@users.noreply.github.com>
2022-01-12 10:28:29 +0000

    Add files via upload

---
+++ guessing game.py
@@ -0,0 +1,8 @@
+import random
+number = random.randint(1, 10)
+print (number)
+guess = int(input("enter a number between 1 and 10: "))
+if guess == number:
+        print("correct")
+        
+  
---
+++ lottery.py
@@ -0,0 +1,12 @@
+import random
+number = random.randint(1, 60)
+numberB = random.randint(1, 5)
+user = int(input("Pick a number 1-60: "))
+user = int(input("Pick a number 1-5: "))
+if number==user:
+    print ('Correct')
+elif numberB==user:
+    print ('You won')
+elif numberB!=user:
+    print ('You lost')
+    
